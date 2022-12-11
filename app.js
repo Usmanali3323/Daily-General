2f059023895b8f54abaf39d26cd5b4c3565ac1a2
Usman <ua0318855@gmail.com>
2022-12-11 18:56:29 +0500

    daily

--- app.js
+++ app.js
@@ -95,6 +95,10 @@ app.get("/posts/:topic", function(req,res){
   }
   posts();
 });
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
